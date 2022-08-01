d7fe2ff8622e82a5a681f4c2a0435228fd3c4a4e
kei-yagi <kei.yagi@gmail.com>
2022-08-01 23:29:58 +0900

    update-basic-usage

--- app/svelvet-app/src/App.svelte
+++ app/svelvet-app/src/App.svelte
@@ -1,45 +1,37 @@
-<script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+<script lang='ts'>
+  import Svelvet from 'svelvet'
+
+  const initialNodes = [
+    {
+      id: 1,
+      position: { x: 50, y: 50 },
+      data: { label: 'Input Node' },
+      width: 175,
+      height: 40,
+      bgColor: 'white'
+    },
+    {
+      id: 2,
+      position: { x: 100, y: 150 },
+      data: { label: 'Default Node' },
+      width: 175,
+      height: 40,
+      bgColor: 'white'
+    },
+    {
+      id: 3,
+      position: { x: 150, y: 250 },
+      data: { label: 'Output Node' },
+      width: 175,
+      height: 40,
+      bgColor: 'white'
+    }
+  ]
+
+  const initialEdges = [
+    { id: 'e1-2', source: 1, target: 2, label: 'edge label' },
+    { id: 'e2-3', source: 2, target: 3 },
+  ]
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Svelvet nodes={initialNodes} edges={initialEdges} background />
